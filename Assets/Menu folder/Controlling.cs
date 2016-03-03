fd30b01253a0e803e0a02bbb231882af9ff76ec4
Haattila <samuel.sitbon@epita.fr>
2016-03-03 03:30:40 +0100

    Added files via upload

---
+++ Assets/Menu folder/Controlling.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Controlling : MonoBehaviour {
+
+    public Transform Menu, optionMenu;
+    public void LoadScen(string name) {
+
+        SceneManager.LoadScene(name);
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+
+    public void options(bool click)
+    {
+        if (click)
+        {
+            optionMenu.gameObject.SetActive(click);
+            Menu.gameObject.SetActive(!click);
+        }
+        else
+        {
+            optionMenu.gameObject.SetActive(click);
+            Menu.gameObject.SetActive(!click);
+        }
+    }
+}
